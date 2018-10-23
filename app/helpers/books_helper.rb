af1b41d5af410027737e5e2d4bbd85589f26f3b7
Aniko T <a.nitto309@gmail.com>
2018-10-23 19:01:12 +0000

    Added two methods for API call, one for random,one for search

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,23 @@
 module BooksHelper
+  
+  def random_photo
+
+    pic = Unsplash::Photo.random(query: "cat",count: 1)
+    #binding.pry
+    picture = [].join 
+      pic.each do |a|
+      picture << a.urls.small
+    end
+    picture
+  end
+  
+  def search_photo(name)
+    pic = Unsplash::Photo.search("#{name}")
+    picture = [].join 
+      pic.each do |a|
+      picture << a.urls.small
+    end
+    picture
+  end
+  
 end
