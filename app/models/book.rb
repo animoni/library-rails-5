3d5ca075e3fdb0f06d6aaead72f7d45a395d3db9
Aniko T <a.nitto309@gmail.com>
2018-10-24 20:34:59 +0000

    Added validation for title and description

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  
+  validates :title, presence: :true, length: { maximum: 50 }
+  validates :description, presence: :true, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
 end
