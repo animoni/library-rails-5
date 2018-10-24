6a62c7c23c5b122811ff3ce1d255594f821b380d
Aniko T <a.nitto309@gmail.com>
2018-10-24 18:13:38 +0000

    Added the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/update'
-  get 'books/edit'
-  get 'books/destroy'
+  root 'home#home' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
