67d3037eac4966318d3af7f0b46e2ec0d1d73417
Aniko T <a.nitto309@gmail.com>
2018-11-01 09:54:40 +0000

    Moved the two methods to picture.rb

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,23 +1,2 @@
 module BooksHelper
-  
-  def random_photo
-
-    pic = Unsplash::Photo.random(query: "cat",count: 1)
-    #binding.pry
-    picture = [].join 
-      pic.each do |a|
-      picture << a.urls.small
-    end
-    picture
-  end
-  
-  def search_photo(name)
-    pic = Unsplash::Photo.search("#{name}")
-    picture = [].join 
-      pic.each do |a|
-      picture << a.urls.small
-    end
-    picture
-  end
-  
 end
