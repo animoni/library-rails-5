30cdd6283562748dd50497589f72407d2f0196b7
Aniko T <a.nitto309@gmail.com>
2018-11-03 10:43:28 +0000

    Added the method for the coloring for the flash messages

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,27 @@
 class AuthorsController < ApplicationController
   
   def index
+    @authors = Author.all
   end
   
   def show
+    #binding.pry
+    @author = Author.find(params[:id])
   end
   
   def new
+    @author = Author.new
   end
   
   def create
+    @author = Author.new(author_params)
+    if @author.save
+      flash[:success] = "New author added."
+      redirect_to author_path(@author.id)
+    else
+      flash[:danger] = "Did you cheked the spelling?"
+      render 'new'
+    end
   end
   
   def edit
@@ -19,5 +31,14 @@ class AuthorsController < ApplicationController
   end
   
   def destroy
+    @author = Author.find(params[:id])
+    @author.delete
+    redirect_to authors_path
+  end
+  
+  private
+  
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
   end
 end
