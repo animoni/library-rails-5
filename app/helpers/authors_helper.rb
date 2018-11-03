3bdb68e8791441ede90455e704a300b8dcaa1c68
Aniko T <a.nitto309@gmail.com>
2018-11-03 10:41:40 +0000

    Added a method to display the full name of the author on the show page

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,11 @@
 module AuthorsHelper
+  
+  def full_name
+    #binding.pry 
+    @author[:first_name] +" "+ @author[:last_name]
+  end
+  
+  def full_name_index(first,last)
+    "#{first} #{last}"
+  end
 end
