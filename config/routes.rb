96554f47e6c7e4bd0ed7aa54c96014b55c609146
Aniko T <a.nitto309@gmail.com>
2018-11-29 19:28:43 +0000

    Don't remember

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :books do
     collection do
        get 'search'
+       post 'picture'
       end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
